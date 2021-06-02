9512c36b081497250d7d0798dc018d6990c12e29
Matěj Chalk <matejchalk@gmail.com>
2021-06-02 21:11:23 +0200

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tdd-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
